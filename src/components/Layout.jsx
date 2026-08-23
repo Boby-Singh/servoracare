d445bea6dbb79679a2ec59a76471ef9bf5c421f5
Boby Singh <126677943+Boby-Singh@users.noreply.github.com>
2026-08-24 00:37:41 +0530

    side bar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,17 @@
 import Navbar from "./Navbar";
-import AdminSidebar from "./AdminSidebar";
 import { Outlet } from "react-router-dom";
 
-function AdminLayout() {
+function Layout() {
   return (
     <>
       <Navbar />
 
-      <div className="min-h-screen overflow-x-hidden">
-        <AdminSidebar />
-
-        <main className="ml-64 min-h-[calc(100vh-5rem)]">
-          <Outlet />
-        </main>
-      </div>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
 
-export default AdminLayout;
+export default Layout;
+
