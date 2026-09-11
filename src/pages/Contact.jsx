86ddb5617621d3e4566d1c7284b187d39775b413
Boby Singh <126677943+Boby-Singh@users.noreply.github.com>
2026-09-11 23:38:27 +0530

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -310,7 +310,7 @@ ${formData.message}`;
                       </p>
 
                       <p className="font-semibold text-slate-900 mt-1">
-                        Aditya Puram, Gwalior, Madhya Pradesh, India, 474005.
+                        Aditya Puram, Gwalior, Madhya Pradesh, 474005.
                       </p>
 
                       <p className="text-sm text-slate-500 mt-1">
@@ -594,7 +594,7 @@ ${formData.message}`;
                     </h2>
 
                     <p className="text-sm text-slate-500">
-                      Adiya Puram, Gwalior, Madhya Pradesh, India, 474005.
+                      Adiya Puram, Gwalior, Madhya Pradesh, 474005.
                     </p>
 
                   </div>
